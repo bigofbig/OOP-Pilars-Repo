6f92e15219b228e6396aae7c801d1a50d23e1863
bigofbig <hoseinstar80@gmail.com>
2022-05-14 23:56:31 +0430

    primary move rotating
    now its rotates based on condition

--- My project/Assets/Scripts/Move.cs
+++ My project/Assets/Scripts/Move.cs
@@ -6,15 +6,61 @@ public class Move : MonoBehaviour
 {
     [SerializeField]BoxCollider obstacleDetectorCollider;
     [SerializeField]Rigidbody capsuleRigidBody;
-    [SerializeField]float MoveSpeed;
+    [SerializeField]float moveSpeed;
+    [SerializeField]float rotateSpeed;
+    [SerializeField]float rotateDelay;
+    [SerializeField]bool isBlocked = false;
+    
     private void Awake()
     {
+      
+
         obstacleDetectorCollider = GetComponent<BoxCollider>();
         capsuleRigidBody = GetComponent<Rigidbody>();
     }
     private void FixedUpdate()
     {
-        capsuleRigidBody.AddRelativeForce(Vector3.forward * MoveSpeed);
+        if (!isBlocked)
+        {
+            capsuleRigidBody.AddRelativeForce(Vector3.forward * moveSpeed);
+        }
+        if (isBlocked) {
+            capsuleRigidBody.constraints = 
+            RigidbodyConstraints.FreezeRotationZ |
+            RigidbodyConstraints.FreezeRotationX;
+            capsuleRigidBody.AddRelativeTorque(Vector3.up*rotateSpeed);
+            print("torqueing");
+        }
+        if (!isBlocked) { capsuleRigidBody.constraints =
+                RigidbodyConstraints.FreezeRotationY|
+                RigidbodyConstraints.FreezeRotationZ |
+                RigidbodyConstraints.FreezeRotationX; 
+            print("Y rotation constrained");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+
+            isBlocked = true;
+            
+        }
+
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        Invoke("IsBlockedSetter", rotateDelay);//delayed, to prevent stucking
         
+
+        
+
+    }
+
+   void IsBlockedSetter()
+    {
+        isBlocked = false;
     }
 }
