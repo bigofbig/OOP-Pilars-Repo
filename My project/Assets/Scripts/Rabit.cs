552ef277fa9e3bd1e3647b7747c8c0574eb0413c
bigofbig <hoseinstar80@gmail.com>
2022-05-17 11:16:45 +0430

    Importing and making rabbit prefab

--- My project/Assets/Scripts/Rabit.cs
+++ My project/Assets/Scripts/Rabit.cs
@@ -7,25 +7,32 @@ public class Rabit : Move //INHERITANCE
    [SerializeField]float jumpForce =200;
    [SerializeField] float jumpDelay = 1;
    float jumpTimer = 0;
+    [SerializeField] Animator animator;
     
     
 
     //POLYMORPHISM
     protected override void MoveForward()
     {
-
+        animator.SetBool("isJumping", false);
         maxVelocity = 10;
         moveForce = 40;
         jumpTimer -= Time.deltaTime;
         if (jumpTimer < 0)
         {
-
+            animator.SetBool("isJumping", true);
             capsuleRigidBody.AddRelativeForce(Vector3.up * jumpForce);
             capsuleRigidBody.AddRelativeForce(Vector3.forward * moveForce);
             jumpTimer = jumpDelay;
+           
+            
         }
+       
+        
+       
         
 
     }
+ 
 
 }
