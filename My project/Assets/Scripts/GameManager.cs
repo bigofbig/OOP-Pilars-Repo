0deab1283c1fcecf5f80523cb00ce46f4e78c92a
bigofbig <hoseinstar80@gmail.com>
2022-05-18 07:39:22 +0430

    adding & creating tree prefab and random spawn system

---
+++ My project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]GameObject rabbit;
+    [SerializeField]GameObject Fox;
+    [SerializeField]GameObject Eagle;
+    [SerializeField]GameObject tree;
+    int rabbitCount;
+    int FoxCount;
+    int EagleCount;
+    int TreeCount;
+
+
+    private void Start()
+    {
+        
+        
+        
+    }
+    private void Update()
+    {
+
+        
+    }
+    void GameObjectSpawner(GameObject spawnGameObject,int spawnCount)
+    {
+        
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Instantiate(spawnGameObject, RandomPos(), RandomRotation());
+        }
+        
+
+    }
+    Vector3 RandomPos()
+    {
+        float zBoundry = Random.Range(-9, 9);
+        float xBoundry = Random.Range(-13, 13);
+
+        Vector3 randomPos = new Vector3(xBoundry, 0, zBoundry);
+        
+
+
+
+        return randomPos;
+    }
+
+    Quaternion RandomRotation()
+    {
+        Quaternion randomRotate = Quaternion.Euler(0, Random.Range(0.0f, 360f),0 );
+        return randomRotate;
+    }
+
+    
+}
